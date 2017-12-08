69ff2b3395e4ab925f72d10df9523ce445c5db59
huajie chen <cmathking@gmail.com>
2017-12-08 17:48:09 +0800

    upgrade version

--- mathwork/路径行走极限问题/path_limit.py
+++ mathwork/路径行走极限问题/path_limit.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
-'''
-在笛卡尔直角坐标系xOy中，有点A(0,1)，OA作为起始向量，从点A出发，到达目的地点B，点B满足：
-(1)AB向量与前一个向量OA的夹角为theta（0<=theta<=π）；
-(2)AB即行走的距离是前一个距离OA的beta（0<=beta<=1）倍；
-下一次行走以向量AB为基准行走至下一目的地，依此类推。
 
-当beta=1, theta=2*pi/n时，所得的图像为正n边形
-当beta=1, theta=pi*a, a越接近于0，图像内圆和外圆接近，a接近1，图像内圆半径接近为0
-''' 
 import numpy as np
 import matplotlib.pyplot as plt 
 import matplotlib.animation as animation 
@@ -19,8 +11,8 @@ beta = 1
 a = 2/7
 theta = math.pi*a
 
-
-def data_gen(): 
+#生成每次行走时的坐标
+def path_gen(): 
     t = 0
     x = 0
     y = 0 
@@ -32,6 +24,7 @@ def data_gen():
             print(x,y)
         yield x, y
 
+#使用plt画出行走时的动态图
 fig, ax = plt.subplots() 
 line, = ax.plot([], [], lw=2)
 if beta == 1:
@@ -41,6 +34,7 @@ else:
     ax.set_ylim(-1/(1-beta), 1/(1-beta))
     ax.set_xlim(-beta/(1-beta), beta/(1-beta)) 
 ax.grid() 
+
 xdata, ydata = [], [] 
 def run(data): 
     x,y = data 
@@ -51,6 +45,8 @@ def run(data):
     # ax.figure.canvas.draw() 
     line.set_data(xdata, ydata) 
     return line, 
-ani = animation.FuncAnimation(fig, run, data_gen, blit=True, interval=100, 
+
+ani = animation.FuncAnimation(fig, run, path_gen, blit=True, interval=100, 
   repeat=False) 
+
 plt.show() 
